4cbebb80ba5a822c35e9ce33c72009c46bdc8488
Mike Pena <mykey737@gmail.com>
2021-07-03 12:53:50 -0400

    Week 3 Workshop Assignment

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -14,7 +14,7 @@ partnerRouter.route("/")
    })
    .catch(err => next(err));
   })
-  .post(authenticate.verifyUser, (req, res, next) => {
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Partner.create(req.body)
     .then(partner => {
       console.log('Partner Created ', partner);
@@ -28,7 +28,7 @@ partnerRouter.route("/")
     res.statusCode = 403;
     res.end("Put operation not supported on /partners");
   })
-  .delete(authenticate.verifyUser, (req, res, next) => {
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Partner.deleteMany()
     .then(response => {
       res.statusCode = 200;
@@ -76,7 +76,7 @@ partnerRouter.route("/:partnerId")
       `POST operation not supported on /partners/${req.params.partnerId}`
     );
   })
-  .put(authenticate.verifyUser, (req, res, next) => {
+  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Partner.findByIdAndUpdate(req.params.partnerId, {
       $set: req.body
     }, {new: true})
@@ -87,7 +87,7 @@ partnerRouter.route("/:partnerId")
     })
     .catch(err => next(err));
   })
-  .delete(authenticate.verifyUser, (req, res, next) => {
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Partner.findByIdAndDelete(req.params.partnerId)
     .then(response => {
       res.statusCode = 200;
--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,16 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router
+.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+  User.find()
+   .then(users => {
+     res.statusCode = 200;
+     res.setHeader('Content-Type', 'application/json');
+     res.json(users);
+   })
+   .catch(err => next(err));
+  });
 
 router.post('/signup', (req, res) => {
   User.register(
@@ -49,18 +56,6 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: "You are successfully logged in!"});
-});
-
-router.get('/logout', (req, res, next) => {
-  if(req.session) {
-    req.session.destroy();
-    res.clearCookie('session-id');
-    res.redirect('/');
-  } else {
-    const err = new Error('You are not logged in!');
-    err.status = 401;
-    return newx(err);
-  }
 });
 
 module.exports = router;
